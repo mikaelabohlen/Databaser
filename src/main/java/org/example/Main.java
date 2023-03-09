898f05e7587ef4368ddf7d952da8b7bcf8ec2ebd
beamer66 <99485685+beamer66@users.noreply.github.com>
2023-03-09 15:10:11 +0100

    Är delaktig

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -2,12 +2,12 @@ package org.example;
 
 
 
+
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.hibernate.cfg.Configuration;
 
-import java.util.ArrayList;
-import java.util.List;
+
 
 public class Main {
     public static void main(String[] args) {
@@ -34,5 +34,9 @@ public class Main {
         session.getTransaction().commit();
         session.close();
 
+        System.out.println("HEJ JAG HETER JIM DETTA ÄR MIN COMMIT TILL DETTA PROGRAMMET");
+
     }
+
+
 }
