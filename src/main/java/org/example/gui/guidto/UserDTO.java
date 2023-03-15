dc1f173384ef6a9a970def17f97fe100fe6728d6
beamer66 <99485685+beamer66@users.noreply.github.com>
2023-03-15 14:52:29 +0100

    mer funktionalitet.

--- src/main/java/org/example/gui/guidto/UserDTO.java
+++ src/main/java/org/example/gui/guidto/UserDTO.java
@@ -1,17 +1,23 @@
 package org.example.gui.guidto;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class UserDTO {
     private String userName;
     private String password;
     private boolean admin;
+    private AdressDTO adressDTO;
+    private List<ConcertDTO> concerts;
 
     public UserDTO() {
-
+        this.concerts = new ArrayList<>();
     }
 
     public UserDTO(String userName, String password) {
         this.userName = userName;
         this.password = password;
+        this.concerts = new ArrayList<>();
     }
 
     public String getUserName() {
@@ -36,5 +42,21 @@ public class UserDTO {
 
     public void setAdmin(boolean admin) {
         this.admin = admin;
+    }
+
+    public AdressDTO getAdressDTO() {
+        return adressDTO;
+    }
+
+    public void setAdressDTO(AdressDTO adressDTO) {
+        this.adressDTO = adressDTO;
+    }
+
+    public List<ConcertDTO> getConcerts() {
+        return concerts;
+    }
+
+    public void setConcerts(List<ConcertDTO> concerts) {
+        this.concerts = concerts;
     }
 }
